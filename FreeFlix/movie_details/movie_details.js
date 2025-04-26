ccaa8fb84923be7d28acd72c9f0f35d4a1a47fe8
CUT1L1Z3R <151803749+CUT1L1Z3R@users.noreply.github.com>
2025-04-26 15:31:28 +0800

    Update movie_details.js

--- FreeFlix/movie_details/movie_details.js
+++ FreeFlix/movie_details/movie_details.js
@@ -88,7 +88,11 @@ async function changeServer() {
     // Ensure iframe is visible and correctly sized
     iframe.style.display = "block";  // Show the iframe
     iframe.style.width = "80%"; // or adjust to a fixed size
-    iframe.style.height = "250px"; // or adjust height as needed
+    iframe.style.height = "300px"; // or adjust height as needed
+    
+    // Hide the movie poster when the video is playing
+    moviePoster.style.display = "none";  // Hide the movie poster image
+}
 
 // Function to display movie details on the page
 async function displayMovieDetails() {
