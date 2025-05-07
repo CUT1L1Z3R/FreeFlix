5b82af71ce3a8f2972cd062c577232ef2d0dc847
CUT1L1Z3R <151803749+CUT1L1Z3R@users.noreply.github.com>
2025-05-08 07:39:26 +0800

    Delete freeflix directory

--- freeflix/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { BannerSlideshow } from "@/components/banner-slideshow";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <BannerSlideshow />
-    </main>
-  );
-}
